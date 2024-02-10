734c78f1f4e1faf68e3c8cbb20f732e6c3422e9e
aarchyparekh25 <153435179+aarchyparekh25@users.noreply.github.com>
2024-02-10 10:49:50 +0530

    Add files via upload
    Datatypes in javascript

---
+++ datatypes.js
@@ -0,0 +1,23 @@
+// Numbers:
+let length = 16;
+let weight = 7.5;
+console.log(typeof(length));
+console.log(typeof(weight));
+
+// Strings:
+let color = "Yellow";
+let lastName = "Johnson";
+console.log(typeof(color));
+console.log(typeof(lastName));
+
+// Booleans
+let x = true;
+let y = false;
+console.log(typeof(x));
+console.log(typeof(y));
+
+var a;
+console.log(a);
+
+var b = null;
+console.log(b);
