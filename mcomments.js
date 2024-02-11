9607b19351ffc1b6835d3331a9cf8d08f51dc553
aarchyparekh25 <153435179+aarchyparekh25@users.noreply.github.com>
2024-02-11 09:28:07 +0530

    Add files via upload

---
+++ mcomments.js
@@ -0,0 +1,8 @@
+/*Here we will learn about 
+multiline comments */
+
+let name = "Aarchy"; 
+let name1 = "Parekh";
+
+console.log("The Multiline comments are commented using /*");
+console.log("Hello " + name + name1);
---
+++ statemnts.js
@@ -0,0 +1,6 @@
+let num , num1 , result; //declaring 3 variables
+num = 15;      //Assign the value 15 to num
+num1 = 7;       //Assign the value 7 to num1
+result = num + num1;  //storing the value of the sum of 2 vaiables
+console.log(result);
+console.log("single line comments example");
