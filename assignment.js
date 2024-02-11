71ea9118328c232e022e6038015fa3dd9cbc7127
aarchyparekh25 <153435179+aarchyparekh25@users.noreply.github.com>
2024-02-11 09:25:36 +0530

    Add files via upload

---
+++ assignment.js
@@ -0,0 +1,25 @@
+let x = 20;
+
+//Addition Assignment Operator
+/*x += 5;
+console.log(x);*/
+
+//Subtraction Assignment Operator
+/*x -= 5;
+console.log(x);*/
+
+//Multiplication Assignment Operator
+/*x *= 5;
+console.log(x);*/
+
+//Division Assignment Operator
+/*x /= 5;
+console.log(x);*/
+
+//Exponention Assignment Operator
+/*x **= 2;
+console.log(x);*/
+
+//Modulous assignment oprator 
+x %= 5;
+console.log(x);
---
+++ comparison.js
@@ -0,0 +1,12 @@
+let num = 20;
+let num1 = 20;
+
+console.log(num == num1);
+console.log(num < num1);
+console.log(num > num1);
+console.log(num <= num1);
+console.log(num >= num1);
+console.log(num === num1);
+console.log(num != num1);
+console.log(num !== num1);
+console.log(num == num1) ? "True" : "False";
